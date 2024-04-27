854f70a7cb6d33c7a35d497dff1b5dd90e00ce3f
Siva G <sivagnair2002@gmail.com>
2024-04-27 18:40:04 +0530

    gy

--- app.py
+++ app.py
@@ -56,7 +56,7 @@ def predict():
                 max_index = np.argmax(predictions)
                 max_value = predictions[0][max_index]
                 percentage = max_value * 100
-                if max_value < 0.3:
+                if max_value < 0.96:
                     return 'Error', percentage
                 else:
                     return class_labels[max_index], percentage
